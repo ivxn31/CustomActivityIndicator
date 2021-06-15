52497b28773cf37655cd9808d72d5e0e37f67049
ivant <ivant@capmega.com>
2021-06-15 17:32:32 -0500

    Initial Commit

---
+++ CustomActivityIndicator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * 15/06/21
+ * Iván Tecpanecatl
+ * ivant@capmega.com
+ */
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomActivityIndicator/CustomActivityIndicatorApp.swift
@@ -0,0 +1,19 @@
+//
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * 15/06/21
+ * Iván Tecpanecatl
+ * ivant@capmega.com
+ */
+
+
+import SwiftUI
+
+@main
+struct CustomActivityIndicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
